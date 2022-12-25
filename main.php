69c17f1ea92ffcd1a4917cd1c12e89f810907f4f
YablokovaValeriya <yablokova.valeryia@yandex.ru>
2022-12-26 02:51:13 +0300

    Second commit

---
+++ main.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <style>
+            html 
+        </style>
+    </head>
+    <body>
+        <div>
+            <h2>Добро пожаловать, <?=@$_COOKIE['user']?>!</h2>
+            <p><a href="logout.php">Выйти</a> из системы</p>
+        </div>
+    </body>
+</html>
+В 
